d92bebf9d6b1c9f9c13b3266398a5dda1a7d4e8e
G3Kappa <g3kappa@gmail.com>
2016-06-21 00:04:03 +0200

    Wrote fetchPostData() and fetchLatestPostIDs() to start working with the back-end

---
+++ doublethink/get_posts.php
@@ -0,0 +1,3 @@
+<?php
+echo '{ "id": 1, "author": "Kevin Giovinazzo", "topic": "politica", "content": "I meme mi piacciono.", "date": "16/06/2016", "avatar_url": "https://scontent.xx.fbcdn.net/v/l/t1.0-9/13096140_594479124047932_3800282213437108589_n.jpg?oh=135e53245a06b47e00240e45d1b15f62&oe=57E64BFF" }'
+?>
--- doublethink/js/doublethink.js
+++ doublethink/js/doublethink.js
@@ -1,24 +1,47 @@
 var Doublethink = new function() {
 
-    /// The makePost function expects _all_ its arguments to be valid.
-    this.makePost = function (category, author, avatar, content, date) {
+    /// The fetchPostData() function expects a number and a callback function
+    /// postID: The ID of the post to fetch data for
+    /// success: The callback function which will be called with a key-value object representing a post as only parameter
+    /// returns: nothing
+    this.fetchPostData = function(postID, success) {
+        // Ask the server to fetch the required data of a post the id of which is postID
+        $.post('get_posts.php', { action: 'fetch-post-data', id: postID }, function(response) {
+            success(JSON.parse(response));
+        });
+    };
+
+    /// The fetchLatestPostIDs() function expects a number and a callback function.
+    /// num: The number of post IDs to fetch
+    /// success: The callback function which will be called with an array of IDs as only parameter
+    /// returns: nothing
+    this.fetchLatestPostIDs = function(num, success) {
+        // Ask the server to return a list of size num containing the latest post IDs
+        $.post('get_posts.php', { action: 'fetch-id-list', num: num }, function(response) {
+            success(JSON.parse(response));
+        });
+    };
+
+    /// The createPost() function expects _all_ its arguments to be valid.
+    /// returns: A constructed "Post" DOM element
+    this.createPost = function (data) {
         // post = Outer frame
         var post = $('<div class="post-full"></div>');
         // Topics change the accent of each post
-        post.addClass('topic-' + category);
+        post.addClass('topic-' + data.topic);
 
         // post_heading = Upper part of the post, complete with avatar and name
         var post_heading = $('<div class="post-heading"><span class="post-topic"></span>' +
-            '<div class="post-avatar"></div><div class="post-author">' + author + '</div></div>');
+            '<div class="post-avatar"></div><div class="post-author">' + data.author + '</div></div>');
 
         // Set the avatar
-        post_heading.find('div.post-avatar').css('background-image', 'url("' + avatar + '")');
+        post_heading.find('div.post-avatar').css('background-image', 'url("' + data.avatar_url + '")');
 
         // post_body = Middle part of the post, contains its main content
-        var post_body = $('<div class="post-body">' + content + '</div>');
+        var post_body = $('<div class="post-body">' + data.content + '</div>');
 
         // post_footer = Bottom part of the post, contains date of posting
-        var post_footer = $('<div class="post-footer">' + date + '</div>');
+        var post_footer = $('<div class="post-footer">' + data.date + '</div>');
 
         post.append(post_heading);
         post.append(post_body);
@@ -27,8 +50,10 @@ var Doublethink = new function() {
         return post;
     };
 
-    this.addPost = function(post) {
-        $('#left-col').append(post);
+    /// The appendPost() function expects an element created via createPost() and appends it to the feed
+    /// returns: nothing
+    this.appendPost = function(dom_element) {
+        $('#left-col').append(dom_element);
     };
 
 };
